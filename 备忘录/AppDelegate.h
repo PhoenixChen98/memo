10c06b367fd2232e5c2614383754128458d7dfa9
Phoenix <Phoenix@PhoenixdeMacBook-Pro.local>
2017-03-28 21:59:51 +0800

    Initial Commit

---
+++ 备忘录/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  备忘录
+//
+//  Created by Phoenix on 2017/3/28.
+//  Copyright © 2017年 Phoenix. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ 备忘录/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  备忘录
+//
+//  Created by Phoenix on 2017/3/28.
+//  Copyright © 2017年 Phoenix. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
